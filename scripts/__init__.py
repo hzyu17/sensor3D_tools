b66430522cf152b3fcc0a387145ce8a971e23960
zinuo <benjaminchang0731@gmail.com>
2025-05-10 14:27:02 -0400

    Added Planar SDF

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -1,3 +1,3 @@
 from .datatype_conversions import PointCloud, OccpuancyGrid, save_occmap_for_matlab, ply_to_mat, transform_sdf
 from .SignedDistanceField3D import generate_field3D
-from .bind_SDF import SignedDistanceField
+from .bind_SDF import SignedDistanceField, PlanarSDF
